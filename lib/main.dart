262e123f3a55f662e7a336d58da1155f6ee438c1
D2A-19 <abyansurya03.com>
2023-12-16 11:54:42 +0700

    Latihan Flutter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lapor_book/firebase_options.dart';
+import 'package:lapor_book/pages/RegisterPage.dart';
+import 'package:lapor_book/pages/SplashPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    title: "Aplikasi Lapor Book",
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      // '/login': (context) => LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      // '/dashboard': (context) => const DashboardPage(),
+      // '/add': (context) => AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
+}
